07ccfa1cacbaa6f36731437959c18adbd5519c86
RameshNaik9 <95227238+RameshNaik9@users.noreply.github.com>
2023-03-30 11:09:17 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Notes from "./Notes";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+      <Notes />
+    </div>
+  );
+}
+
+export default App;
